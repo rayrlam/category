d3eb23b3f95a2e8bf86d19f345c490abdc09613c
Ray <rayrlam@yahoo.com.hk>
2022-05-15 18:42:30 +0100

    add static & docker

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -5,18 +5,18 @@ use App\Models\Category;
 
 class CategoryRepository
 {
-    public function getCategory(int $id): array
+    static public function getCategory(int $id): array
     {
         $category = Category::findOrFail($id);
         return ['id'=>$category->id, 'parent_id'=>$category->parent_id, 'name'=>$category->name];
     }
 
-    public function categories($id): object
+    static public function categories($id): object
     {
         return Category::where('parent_id', $id)->get();
     }
 
-    public function list(): object
+    static public function list(): object
     {
         return Category::all();
     }
