9d62a673aaea1748b56fef827f0675a1e5dfb3af
Ray <rayrlam@yahoo.com.hk>
2022-05-28 16:28:45 +0100

    update

--- resources/views/breadcrumbs.blade.php
+++ resources/views/breadcrumbs.blade.php
@@ -7,11 +7,7 @@
         </div>
     </x-slot>
 
-<<<<<<< HEAD
-    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
-=======
     <div class="p-8 w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
->>>>>>> ff2406772efa7c87060acd0723be739f71af6783
         <div class="mt-4">
             @if(isset($breadcrumbs))
                 <x-breadcrumb :breadcrumbs="$breadcrumbs" />
